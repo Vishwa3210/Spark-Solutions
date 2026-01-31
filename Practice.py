8df22d8a1eb002c652d2c93af21fdbf54012fc6e
Vishwa3210 <148730645+Vishwa3210@users.noreply.github.com>
2026-01-31 13:34:07 -0500

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -1,9 +1,11 @@
+# Initialize Spark session
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
 
-df_result = spark.read.format('csv').option('header','true').option('inferSchema','true').load('/datasets/customers.csv')
+# Load the file
+df_result = spark.read.format('csv').option('header','true').option('inferSchema','true').load('/datasets/customers_raw.csv')
 
-# Chain filters together
-df_result = df_result.filter(df_result.purchase_amount > 100.00).filter(df_result.age >= 30)
-df_result=df_result.select('customer_id','name','purchase_amount')
+# REASSIGN each transformation
+df_result = df_result.dropna()
+# Display the final DataFrame
 display(df_result)
